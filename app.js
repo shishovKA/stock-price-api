e8bf58107ae34a15940a022b2e89369ddf24d5e3
Konstantin Shishov <shishov.k.a@yandex.ru>
2022-04-11 10:48:33 +0300

    change port to 8080

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello Митильба!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
